eb8f52584c87f49a1799d19320575d8ecbe602d3
Stefano Cilloni <dev@nyxgear.com>
2026-03-19 09:53:54 +0000

    feat: npm + alias up

--- oh-my-zsh/custom/zshrc-startup.zsh
+++ oh-my-zsh/custom/zshrc-startup.zsh
@@ -16,12 +16,20 @@ alias d=docker
 alias tf=terraform
 alias weather='f(){ curl v2.wttr.in"$@";  unset -f f; }; f'
 alias cat='bat'
-
+alias up='git ci --amend --no-edit . && git ps -f' # useful to run precommit hooks after git ci --no-verify
 
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/#enable-shell-autocompletion
 source <(kubectl completion zsh)
 complete -F __start_kubectl k
 
+# ------------------------- NPM --------------------------------
+
+# Set an '_auth' configuration value for npm.
+# - Since yarn cannot use scoped auth, we must provide this bare '_auth'
+#   configuration value so that yarn can access workday artifactory.
+# - This allows 'yarn' to use the same '~/.npmrc' file as 'npm' and 'pnpm'.
+export npm_config__auth="$(sed -n -e "s!.*_auth *= *\(.*\)!\1!p" ~/.npmrc)"
+
+
 # source the work zshrc-startup.zsh if it exists
 source ~/.dotfiles-work/oh-my-zsh/custom/zshrc-startup-work.zsh 2>/dev/null || true
-
